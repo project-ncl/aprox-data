e9caa48852e834679bb9d22162f817ccd00d8555
John Casey <jdcasey@commonjava.org>
2014-12-02 17:01:35 -0600

    Initial commit with two rules, for creating repos related to products, and repos related to builds.

---
+++ autoprox/0005-build-repos.groovy
@@ -0,0 +1,48 @@
+
+import org.commonjava.aprox.autoprox.data.*;
+
+import java.net.MalformedURLException;
+
+import org.commonjava.aprox.model.*;
+
+class BuildRepositoryRule extends AbstractAutoProxRule
+{
+    boolean matches( String name ){
+        // format: build-<prod-name>-<prod-ver>-<proj-name>-<number>
+        name.startsWith( "build+" ) && name.split('+').length == 5
+    }
+
+    HostedRepository createHostedRepository( String named )
+    {
+        HostedRepository r = new HostedRepository( named )
+        r.setAllowSnapshots( true )
+        r.setAllowReleases( true )
+
+        r
+    }
+
+    Group createGroup( String named )
+    {
+        def match = named.split('+')
+        //def buildPrefix = match[0]
+
+        def prodName = match[1]
+        def prodVer = match[2]
+        def projName = match[3]
+        def projBuild = match[4]
+
+        Group g = new Group( named );
+        g.addConstituent( new StoreKey( StoreType.remote, named ) )
+        g.addConstituent( new StoreKey( StoreType.hosted, named ) )
+        
+        g
+    }
+
+    RemoteRepository createRemoteRepository( String name )
+        throws AutoProxRuleException, MalformedURLException
+    {
+        def match = named.split('+')
+        new RemoteRepository( name, 'http://localhost/api/group/product-${match[1]}-${match[2]}' );
+    }
+}
+
---
+++ autoprox/0006-prod-repos.groovy
@@ -0,0 +1,45 @@
+
+import org.commonjava.aprox.autoprox.data.*;
+
+import java.net.MalformedURLException;
+
+import org.commonjava.aprox.model.*;
+
+class ProductRepositoryRule extends AbstractAutoProxRule
+{
+    boolean matches( String name ){
+        // format: product+<prod-name>+<prod-ver>
+        name.startsWith( "product+" ) && name.split('+').length == 3
+    }
+
+    HostedRepository createHostedRepository( String named )
+    {
+        HostedRepository r = new HostedRepository( named )
+        r.setAllowSnapshots( true )
+        r.setAllowReleases( true )
+
+        r
+    }
+
+    Group createGroup( String named )
+    {
+        def match = named.split('+')
+        //def productPrefix = match[0]
+
+        def prodName = match[1]
+        def prodVer = match[2]
+
+        Group g = new Group( named );
+        g.addConstituent( new StoreKey( StoreType.remote, named ) )
+        g.addConstituent( new StoreKey( StoreType.hosted, named ) )
+        
+        g
+    }
+
+    RemoteRepository createRemoteRepository( String name )
+        throws AutoProxRuleException, MalformedURLException
+    {
+        new RemoteRepository( name, 'http://localhost/api/group/public' );
+    }
+}
+
